d35d03f46942122e4c7a19bf0b4e9559700b4e76
SDMckone <seandmckone@gmail.com>
2023-10-13 10:35:16 -0400

    fix filepaths

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import "./App.css";
+import img1 from "./assets/images/image1.jpg";
+import img2 from "./assets/images/image2.jpg";
+import img3 from "./assets/images/image3.jpg";
+import img4 from "./assets/images/image4.jpg";
+import img5 from "./assets/images/image5.jpg";
+import img6 from "./assets/images/image6.jpg";
+import img7 from "./assets/images/image7.jpg";
+import img8 from "./assets/images/image8.jpg";
+import img9 from "./assets/images/image9.jpg";
+import img10 from "./assets/images/image10.jpg";
+import img11 from "./assets/images/image11.jpg";
+import img12 from "./assets/images/image12.jpg";
 
 import { useState, useEffect } from "react";
 import Card from "./components/Card";
@@ -8,18 +20,18 @@ function App() {
   const [highScore, setHighScore] = useState(0);
   const [clickedIds, setClickedIds] = useState([]);
   const [imageList, setImageList] = useState([
-    "../src/assets/images/image1.jpg",
-    "../src/assets/images/image2.jpg",
-    "../src/assets/images/image3.jpg",
-    "../src/assets/images/image4.jpg",
-    "../src/assets/images/image5.jpg",
-    "../src/assets/images/image6.jpg",
-    "../src/assets/images/image7.jpg",
-    "../src/assets/images/image8.jpg",
-    "../src/assets/images/image9.jpg",
-    "../src/assets/images/image10.jpg",
-    "../src/assets/images/image11.jpg",
-    "../src/assets/images/image12.jpg",
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
   ]);
 
   useEffect(() => {
