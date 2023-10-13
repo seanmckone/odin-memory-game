49908b29e13687c6c29de9ac7f171aed6ac0a22a
SDMckone <seandmckone@gmail.com>
2023-10-12 20:40:49 -0400

    New css file import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/reset.css";
-import "./index.css";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
